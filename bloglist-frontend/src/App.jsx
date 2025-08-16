bd807c8193a93370ca50e8bbc94c47197db7399e
AWake01 <40424954+AWake01@users.noreply.github.com>
2025-08-16 22:15:09 +0100

    Exercise 5.2 complete: Persistant logon/logoff in local storage

--- bloglist-frontend/src/App.jsx
+++ bloglist-frontend/src/App.jsx
@@ -1,9 +1,15 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
+import axios from 'axios'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  //Login
+  const [username, setUsername] = useState([])
+  const [password, setPassword] = useState([])
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,14 +17,67 @@ const App = () => {
     )  
   }, [])
 
-  return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
+  useEffect(() => {   //Set user intialy from local storage if user was previosly logged in
+    const loggedInUser = window.localStorage.getItem('currentUser')
+    if(loggedInUser) {
+      const user = JSON.parse(loggedInUser)
+      setUser(user)
+    }
+  }, [])
+
+  const handleLogin = async (event) => {  //Called by login button
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({ username, password }) //Get user returned with valid id from login form
+
+      setUser(user)
+      setUsername('')
+      setPassword('')
+      console.log(`${user.name} logged in`)
+      window.localStorage.setItem("currentUser", JSON.stringify(user))
+    } catch (exception){
+      console.log("Wrong credidentials")
+      //TODO: add notification and timer
+    }
+  }
+
+  const handleLogout = async (event) => {  //Called by logout button
+    event.preventDefault()
+    window.localStorage.removeItem("currentUser")
+    setUser(null)
+  }
+
+  const loginForm = () => (  //Show only when user is not logged in
+      <form onSubmit={handleLogin}>
+        <div>username <input type='text' value={username} name={"username"} onChange={({ target }) => setUsername(target.value)}/></div>
+        <div>password <input type='text' value={password} name={"password"} onChange={({ target }) => setPassword(target.value)}/></div>
+        <button type="submit">login</button>
+      </form>
   )
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>blogs</h2>
+        <form onSubmit={handleLogin}>
+          <div>username <input type='text' value={username} name={"username"} onChange={({ target }) => setUsername(target.value)}/></div>
+          <div>password <input type='text' value={password} name={"password"} onChange={({ target }) => setPassword(target.value)}/></div>
+          <button type="submit">login</button>
+        </form>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <h2>blogs</h2>
+        <div><label>{`${user.name} has logged in `}</label><input type='button' value="log out" onClick={handleLogout}/></div>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </div>
+    )
+  }
 }
 
 export default App
